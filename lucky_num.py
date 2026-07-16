f1a107337e65bbb984f39fe2cafb930e24bc4c09
Abhisheksharma <abhishek78.mc@gmail.com>
2026-07-16 21:22:20 +0530

    Update lucky_num.py

--- lucky_num.py
+++ lucky_num.py
@@ -1,9 +1,18 @@
+# n = int(input())
+
+# a = n // 10
+# b = n % 10
+
+# if a % b == 0 or b % a == 0:
+#     print("YES")
+# else:
+#     print("NO")
 n = int(input())
 
 a = n // 10
 b = n % 10
 
-if a % b == 0 or b % a == 0:
+if (a != 0 and b % a == 0) or (b != 0 and a % b == 0):
     print("YES")
 else:
     print("NO")
