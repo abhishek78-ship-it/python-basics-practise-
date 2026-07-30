080d0785b89685d3c6f853132014a0cc103c96e8
Abhisheksharma <abhishek78.mc@gmail.com>
2026-07-30 19:48:48 +0530

    Add files via upload

---
+++ sorting.py
@@ -0,0 +1,10 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+# Bubble Sort
+for i in range(n):
+    for j in range(n - i - 1):
+        if a[j] > a[j + 1]:
+            a[j], a[j + 1] = a[j + 1], a[j]
+
+print(*a)
