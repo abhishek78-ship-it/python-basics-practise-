d455b50956bcc5412199cb3c44bc1b6022c45f16
Abhisheksharma <abhishek78.mc@gmail.com>
2026-09-13 12:39:11 +0530

    Update Fibonacci calculation method
    Refactor Fibonacci sequence implementation to use an iterative approach and store results in a list.

--- easy_fibonaci.py
+++ easy_fibonaci.py
@@ -1,5 +1,28 @@
-n= int(input("Enter a number: "))
-a, b = 0, 1
-for i in range(n):
-     print(a, end=' ')
-     a, b = b, a + b
+# n= int(input("Enter a number: "))
+# a, b = 0, 1
+# for i in range(n):
+#    print(a, end=' ')
+#    a, b = b, a + b
+#  failed error
+
+# def fib(n):
+#     if n == 1:
+#         return 0
+#     if n == 2:
+#         return 1
+#     return fib(n - 1) + fib(n - 2)
+
+# n = int(input())
+
+# for i in range(1, n + 1):
+#     print(fib(i), end=" ")
+# time limit exceed error
+
+n = int(input())
+
+fib = [0, 1]
+
+for i in range(2, n):
+    fib.append(fib[i - 1] + fib[i - 2])
+
+print(*fib[:n])
