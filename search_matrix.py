04edf907095f9d8b266d90456b05520f6429fd69
Abhisheksharma <abhishek78.mc@gmail.com>
2026-09-24 20:48:49 +0530

    Add initial search_matrix.py implementation

---
+++ search_matrix.py
@@ -0,0 +1,7 @@
+n, m = map(int, input().split())
+found = False
+for i in range(n):
+    arr = list(map(int, input().split()))
+    for j in range(m):
+        if arr[j] == int(input()) if False else False:
+            pass
